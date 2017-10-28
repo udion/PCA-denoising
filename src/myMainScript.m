1cb52511a04e01b24aca761b9135fc468a8138c4
udion <uddeshya.upa@gmail.com>
2017-10-28 13:47:28 +0530

    improved implementation by 200X in speed

--- src/myMainScript.m
+++ src/myMainScript.m
@@ -28,21 +28,49 @@ subplot(1,3,1), imshow(I1,[]), title('orignal');
 subplot(1,3,2), imshow(I1_,[]), title('corrupt');
 subplot(1,3,3), imshow(rec1, []), title('denoised');
 savefig('../images/PCAdenoise1');
-
+pause(0.5);
 
 %% Reconstruction from corrupt Image, technique 2
 % This technique, takes above mentioned average only over certain 'similar'
 % patches and hence is expected to perform better than the previous one 
-rec2 = myPCADenoising2(I2_, 31, 31, 7, 7, 200, sigma);
-diff2 = (double(I2)-rec2).^2;
+rec2 = myPCADenoising2(I1_, 31, 31, 7, 7, 200, sigma);
+diff2 = (double(I1)-rec2).^2;
 % The error RSMD error of reconstructed image is
 RSMD2 = sqrt(sum(diff2(:)));
 
 figure;
 colormap('gray');
-subplot(1,3,1), imshow(I2,[]), title('orignal');
-subplot(1,3,2), imshow(I2_,[]), title('corrupt');
+subplot(1,3,1), imshow(I1,[]), title('orignal');
+subplot(1,3,2), imshow(I1_,[]), title('corrupt');
 subplot(1,3,3), imshow(rec2, []), title('denoised');
 savefig('../images/PCAdenoise2');
+pause(0.5);
+
+%% Results with Bilateral Filtering
+% This method denoises the above corrupted image using the bilateral
+% filtering, note that the performance here does depend upon the tuning of
+% the 2 parameters corresponding to the sigma of space kernel and that of
+% intensity kernel and hence is very subjective.
+s1 = 10;
+s2 = 24;
+[rec3, Ks] = myBilateralFiltering(I1_,s1,s2,floor(3*s1));
+
+diff3 = (double(I1)-rec3).^2;
+% The error RSMD error of reconstructed image is
+RSMD3 = sqrt(sum(diff3(:)));
+
+figure;
+colormap('gray');
+subplot(1,3,1), imshow(I1,[]), title('orignal');
+subplot(1,3,2), imshow(I1_,[]), title('corrupt');
+subplot(1,3,3), imshow(rec3, []), title('denoised');
+savefig('../images/BLdenoise3');
+pause(0.5);
+
+%% RSMD, good enough?
+% As can be seen in the above results, Even though the ouput of technique 2
+% is visually more appealing than the output of Bilateral filtering, RSMD
+% of bilateral filtered image is the least, hence if one can not blindly
+% believs in RSMD, and it's not a good enough measure.
 
 toc;
--- src/myPCADenoising2.m
+++ src/myPCADenoising2.m
@@ -5,11 +5,13 @@ function [oImg,A] = myPCADenoising2(iImg, sr, sc, r, c, K, sigma)
     %display(sz);
     A = zeros(sz(1), sz(2), 2);
     sigma2 = sigma^2;
+    P = splicerVectoriser(iImg, r, c);
     for i=1:sz(1)-r+1
         %display(sz(2))
         for j=1:sz(2)-c+1
             %display(j)
-            P_i = similarPatchSpace(iImg, sr, sc, r, c, K, i, j);
+            %P_i = similarPatchSpace(iImg, sr, sc, r, c, K, i, j);
+            P_i = test(P, sr, sc, sz(1), sz(2), r, c, K, i, j);
             [V, ~] = eig(P_i*P_i');
             V = normc(V);
             Coeff = P_i'*V;
